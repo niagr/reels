34603138f3b2c8a3e7f3933600932f15fdddaa9c
Nishant George Agrwal <nishantagrwal12993@gmail.com>
2014-12-06 14:44:03 +0000

    Fixed permissions issue with windows install script.

--- install-win.sh
+++ install-win.sh
@@ -3,6 +3,7 @@
 # Installs Reels into C:/Program Files/Reels.
 # Run with cygwin as admin. Needs the unzip package.
 # This will not add the executable to PATH. Create a shorcut.
+# NOTICE: You might need to run dos2unix on this first.
 
 # Make install dir
 mkdir /cygdrive/c/Program\ Files/Reels
@@ -11,10 +12,13 @@ mkdir /cygdrive/c/Program\ Files/Reels
 cp -R `ls | grep -v bin` /cygdrive/c/Program\ Files/Reels/
 
 # Deconpress the node-webkit package
-unzip bin/node-webkit-v0.11.2-win-x64.zip -d bin
+unzip bin/node-webkit-v0.11.2-win-x64.zip -d bin/
 
 # Copy the node-webkit binaries into the install dir
 cp -R bin/node-webkit-v0.11.2-win-x64/* /cygdrive/c/Program\ Files/Reels/
+
+# Fix permissions
+chmod -R ugo+rx /cygdrive/c/Program\ Files/Reels
 
 # Rename the 'nw' executable to 'reels'
 mv /cygdrive/c/Program\ Files/Reels/nw /cygdrive/c/Program\ Files/Reels/reels
